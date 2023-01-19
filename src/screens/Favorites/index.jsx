4b3dc4ca3443eaf0e7bdfa9e3831c5a2afef6463
kauanny-cmd <kauannyvieira8@gmail.com>
2023-01-19 19:24:22 -0300

    feat: pages padrões

---
+++ src/screens/Favorites/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+const Favorites = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Favorites</Text>
+        </View>
+    );
+}
+export default Favorites;
---
+++ src/screens/Historic/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+const Historic = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Historic</Text>
+        </View>
+    );
+}
+export default Historic;
