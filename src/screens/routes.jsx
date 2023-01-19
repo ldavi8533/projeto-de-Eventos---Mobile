1800e99b678ecbee827b8e1425fd3ea46077a23d
kauanny-cmd <kauannyvieira8@gmail.com>
2023-01-19 19:24:41 -0300

    feat: uso de rotas e navegação

---
+++ src/screens/routes.jsx
@@ -0,0 +1,89 @@
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import Home from "./Home";
+import Favorites from './Favorites';
+import Profile from './Profile';
+import Historic from './Historic';
+
+import Colors from '../style/colors';
+
+const Tab = createBottomTabNavigator()
+
+const Routes = () => {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+                screenOptions={{
+                    headerShown: false,
+                    tabBarActiveTintColor: Colors.secondaryColor,
+                    tabBarInactiveTintColor: Colors.primaryColor
+                }}
+            >
+                <Tab.Screen name="Home"
+                    component={Home}
+                    options={{
+                        tabBarIcon: ({ focused, color, size }) => (
+                            <Ionicons
+                                name={
+                                    focused
+                                        ? 'ios-home'
+                                        : 'ios-home-outline'
+                                }
+                                size={size}
+                                color={color}
+                            />
+                        )
+                    }}
+                />
+                <Tab.Screen name="Favorites" component={Favorites}
+                    options={{
+                        tabBarIcon: ({ focused, color, size }) => (
+                            <Ionicons
+                                name={
+                                    focused
+                                        ? 'ios-heart'
+                                        : 'ios-heart-outline'
+                                }
+                                size={size}
+                                color={color}
+                            />
+                        )
+                    }}
+                />
+                <Tab.Screen name="Histórico" component={Historic}
+                    options={{
+                        tabBarIcon: ({ focused, color, size }) => (
+                            <Ionicons
+                                name={
+                                    focused
+                                        ? 'ios-list'
+                                        : 'list'
+                                }
+                                size={size}
+                                color={color}
+                            />
+                        )
+                    }}
+                />
+                <Tab.Screen name="Perfil" component={Profile}
+                    options={{
+                        tabBarIcon: ({ focused, color, size }) => (
+                            <Ionicons
+                                name={
+                                    focused
+                                        ? 'ios-person'
+                                        : 'ios-person-outline'
+                                }
+                                size={size}
+                                color={color}
+                            />
+                        )
+                    }}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
+export default Routes;
