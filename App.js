bcaa851cfd8c7eebb10d442ea14e469a2aa162c5
kauanny-cmd <kauannyvieira8@gmail.com>
2023-01-24 21:35:55 -0300

    feat: status bar

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
+import {StatusBar} from 'react-native';
 
 import AuthNavigator from './src/navigations/authNavigator';
 
@@ -6,6 +7,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <AuthNavigator />
+      <StatusBar barStyle='dark-content' backgroundColor='transparent' translucent/>
     </NavigationContainer>
   );
 }
