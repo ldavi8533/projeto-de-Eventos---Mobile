06ddf03f9aa65464921f6168eecb377f4ea64bbb
lucasttavares <zlucas.np@gmail.com>
2023-02-06 11:03:53 -0300

    feat: cria componente para inputs de texto

---
+++ src/components/EditProfileLabel/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+
+export function Heading(Props) {
+
+    const [text, onChange] = React.useState('');
+
+  return (
+    <View style={styles.container}>
+        <Text style={styles.labelTitle}>
+            {Props.labelTitle}
+        </Text>
+
+        <TextInput 
+            style={styles.labelSubtitle}
+            onChange={onChange}
+            value={text}
+            placeholder={Props.labelSubtitle}
+            placeholderTextColor='#377481'
+        />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    labelTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        paddingBottom: 5
+    },
+    labelSubtitle:{
+        fontSize: 20,
+        paddingBottom: 10
+    }
+})
