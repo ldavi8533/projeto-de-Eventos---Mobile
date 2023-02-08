c88ff529a52f2fe36371ff1b2016b2660b6385b4
lucasttavares <zlucas.np@gmail.com>
2023-02-08 20:46:01 -0300

    view: melhora componente

--- src/components/EditProfileLabel/index.jsx
+++ src/components/EditProfileLabel/index.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export function EditProfileLabel(Props) {
-
-    const [text, onChange] = React.useState('');
 
   return (
     <View style={styles.container}>
         <Text style={styles.labelTitle}>
             {Props.labelTitle}
         </Text>
-
-        <TextInput 
-            style={styles.labelSubtitle}
-            onChange={onChange}
-            value={text}
-            placeholder={Props.labelSubtitle}
-            placeholderTextColor='#377481'
-        />
     </View>
   );
 }
@@ -26,10 +16,6 @@ const styles = StyleSheet.create({
     labelTitle: {
         fontSize: 20,
         fontWeight: 'bold',
-        paddingBottom: 5
-    },
-    labelSubtitle:{
-        fontSize: 20,
-        paddingBottom: 10
+        marginLeft: 38
     }
 })
