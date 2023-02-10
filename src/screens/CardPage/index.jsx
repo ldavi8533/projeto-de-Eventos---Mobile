20d0f828fe6d968ffd18a1ba5c825932dd708e4e
nameNosyer <reysonferreira2018@gmail.com>
2023-02-10 14:34:32 -0300

    feat: card page  editing

--- src/screens/CardPage/index.jsx
+++ src/screens/CardPage/index.jsx
@@ -1,94 +1,33 @@
-import { useState } from 'react';
-import { Ionicons } from '@expo/vector-icons';
-import { View, Text, Modal, Pressable, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import { View, ScrollView} from 'react-native';
 
 import Button from '../../components/Button';
 
 import Colors from '../../style/colors';
 
+import InfoCardPage from '../../components/InfoCardPage'
+
 const CardPage = ({ navigation }) => {
-    const [modalVisible, setModalVisible] = useState(false);
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View backgroundColor='white'>
+            
 
-            <Text>CardPage</Text>
+            <ScrollView maximumZoomScale={true}>
 
-            <View >
-                <Pressable onPress={() => setModalVisible(true)}>
-                    <Ionicons name='location' size={30} />
-                    <Text>Visualizar local</Text>
-                </Pressable>
+                <InfoCardPage />
 
-                <Modal
-                    transparent={true}
-                    visible={modalVisible}
-                >
-                    <View style={[styles.centeredView, { backgroundColor: 'rgba(255,255,255,0.75)' }]} >
-                        <View style={styles.modalView}>
-                            <Pressable
-                                style={[styles.button, styles.buttonClose]}
-                                onPress={() => setModalVisible(!modalVisible)}>
-                                <Ionicons name='close' />
-                            </Pressable>
-
-                            <MapView
-                                style={[styles.map, { width: 300, height: 450 }]}
-                            />
-
-                            <Text style={styles.modalText}>Local</Text>
-                        </View>
-                    </View>
-                </Modal>
-
-            </View>
-
-            <Button title='Comprar ingresso'
-                colorBorder={Colors.primaryColor}
-                colorButton={Colors.primaryColor}
-                colorText={Colors.whiteColor}
-                onPress={() => navigation.navigate('TicketPage')}
-            />
-
+                <View style={{ paddingLeft:30, paddingRight:30, paddingBottom:10 }}>
+                    <Button title='Comprar ingresso'
+                    colorBorder={Colors.primaryColor}
+                    colorButton={Colors.primaryColor}
+                    colorText={Colors.whiteColor}
+                    onPress={() => navigation.navigate('TicketPage')} 
+                    />
+                </View>
+            
+            </ScrollView>
         </View>
+
     );
 }
 export default CardPage;
-
-const styles = StyleSheet.create({
-    centeredView: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    modalView: {
-        backgroundColor: 'white',
-        borderRadius: 20,
-        padding: 10,
-        alignItems: 'center',
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
-    },
-    button: {
-        borderRadius: 20,
-        padding: 10,
-        elevation: 2,
-    },
-    buttonClose: {
-        marginBottom: 10,
-        backgroundColor: '#2196F3',
-    },
-    modalText: {
-        padding: 10,
-        textAlign: 'justify',
-    },
-    map: {
-        padding: 10,
-    },
-});
